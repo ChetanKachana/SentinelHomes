36dbb47e5149be345dd5cdfd68aa54e8215899f4
ChetanKachana <chetan.kachana@gmail.com>
2026-05-16 15:35:15 -0700

    Initial Commit

---
+++ SentinelHomes/SentinelHomesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelHomesApp.swift
+//  SentinelHomes
+//
+//  Created by Chey K on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SentinelHomesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SentinelHomesTests/SentinelHomesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelHomesTests.swift
+//  SentinelHomesTests
+//
+//  Created by Chey K on 5/16/26.
+//
+
+import Testing
+@testable import SentinelHomes
+
+struct SentinelHomesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
